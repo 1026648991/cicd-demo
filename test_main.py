65100fff5d65f5e2f1e8eebd59cb3240fe97db55
孙胜 <1026648991@qq.com>
2026-04-15 17:56:41 +0800

    第一次提交

---
+++ test_main.py
@@ -0,0 +1,18 @@
+# test_main.py
+import unittest
+from main import add, subtract
+
+class TestMathOperations(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(-1, 1), 0)
+
+    def test_subtract(self):
+        self.assertEqual(subtract(5, 3), 2)
+        self.assertEqual(subtract(0, 0), 0)
+
+    def test_add(self):
+        self.assertEqual(add(2,2),5)
+
+if __name__ == '__main__':
+    unittest.main()
